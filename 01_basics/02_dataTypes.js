c338e643de8f36b00b09d81bf224524054a87c6d
manpreetjord <30529028+manpreetjord@users.noreply.github.com>
2023-09-25 07:53:02 +0000

    Conversion of DataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -3,5 +3,10 @@
 //console.log(3+3) console.log("Manpreet")// Bad code readability
 console.log(3+
     3);//code readability should be high
- console.log("Manpreet")
- 
+ console.log("Manpreet") 
+ console.log(typeof(null))  //object
+ console.log(typeof(undefined))//undefined
+ console.log(typeof("Hello"));//string
+ console.log(typeof(5));//number
+console.log(typeof(true));//boolean
+console.log(typeof({student:{name:"Manpreet",age:"29"}}));//object
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score="33";
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+let valueInNumber=Number(score);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33"=> 33
+//"33abc"=>NaN
+//true=>1;
+//false=>0;
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(typeof(booleanIsLoggedIn));
+console.log(booleanIsLoggedIn);
