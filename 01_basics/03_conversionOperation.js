db690bf6d9b4ad6ba49ec22b5a254e8ebec27bca
manpreetjord <30529028+manpreetjord@users.noreply.github.com>
2023-09-25 13:43:48 +0000

    Prefix PostFix

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,5 +14,48 @@ let valueInNumber=Number(score);
 
 let isLoggedIn=1;
 let booleanIsLoggedIn=Boolean(isLoggedIn);
-console.log(typeof(booleanIsLoggedIn));
-console.log(booleanIsLoggedIn);
+// console.log(typeof(booleanIsLoggedIn));
+// console.log(booleanIsLoggedIn);
+
+//*******************************Operations*********************************
+
+let value=3;
+let negValue= -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello";
+let str2=" Manpreet";
+let str3=str1+str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+
+//Prefix and Postfix Operators
+
+// let gameCounter=100;
+// gameCounter++; 
+// console.log(gameCounter);//101
+
+
+let x=3;
+const y=x++;
+console.log(x, y); //4,3
+
+let a=3;
+const b=++a;
+console.log(a, b); //4,4
