61dbee1376d036fd60470f9498a1bad512420f5d
manpreetjord <30529028+manpreetjord@users.noreply.github.com>
2023-09-25 07:33:56 +0000

    variables_datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=144553; //Cant change the value when using const
+let accountEmail="manpreetjord@gmail.com"
+var accountPassword="12345"
+accountCity="Pune"
+let accountState
+
+
+// accountId=2
+console.log(accountId);
+accountEmail="ma@as.com"
+accountPassword="121212"
+accountCity="Bangalore"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
+/*
+Preffer not to use var as it has global scope */
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict"
+// alert(3+3) we are using nodejs,not browser
+//console.log(3+3) console.log("Manpreet")// Bad code readability
+console.log(3+
+    3);//code readability should be high
+ console.log("Manpreet")
+ 
