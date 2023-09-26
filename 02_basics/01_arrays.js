9a7e820dfafd6db84ce6040a7d6a63b6d7e2abc8
manpreetjord <30529028+manpreetjord@users.noreply.github.com>
2023-09-26 18:22:46 +0000

    Arrays in JS

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,6 @@
+//Arrays
+//Arrays do Shallow Copies. It is a copy that shares the same reference points
+const myArray=[1,2,3,4,5,true,"Jord"];
+
+console.log(myArray.length); 
+console.log(myArray[2]);
