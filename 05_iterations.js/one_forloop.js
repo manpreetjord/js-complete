709f7212f3465ecab8f22733e0e52da7fcc30ccd
manpreetjord <30529028+manpreetjord@users.noreply.github.com>
2023-09-29 09:18:52 +0000

    for,while and do loop

---
+++ 05_iterations.js/one_forloop.js
@@ -0,0 +1,49 @@
+//for
+
+// for (let i = 0; i <10; i++) {
+//     const element = i;
+    
+//     if (element==5) {
+//         // console.log("5 is Best number");
+//     }
+//     // console.log(element);
+// }
+
+// // console.log(element);
+
+// for (let i = 1; i <= 10; i++) {
+//    // console.log(`outer loop value:  ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`inner loop value ${j} and inner loop ${i}`);
+//        // console.log(i+'*' + j + '=' + i*j);
+        
+        
+//     }
+    
+// }
+
+// let myArray=["flash","batman","thor"]
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+
+//break and continue
+
+// for (let index = 1; index < 20; index++) {
+//     if (index==5) {
+//         console.log("Detected 5");
+//         break;
+//     }
+//     console.log((`Value of i is : ${index}`))
+    
+// }
+for (let index = 1; index < 20; index++) {
+    if (index==5) {
+        console.log(`Detected 5`);
+        continue;
+    }
+    console.log((`Value of i is : ${index}`))
+    
+}
---
+++ 05_iterations.js/two_while_dowhile.js
@@ -0,0 +1,23 @@
+//while
+
+// let index=0
+// while (index<=10) {
+    
+//     console.log(`Value of index is: ${index}`);
+//     index=index+2;
+// }
+
+// let myArray=[`flash`, `batman`,`superman`];
+// let heroArr=0;
+// while(heroArr<myArray.length){
+//     console.log(`Value of array is ${myArray[heroArr]}`);
+//     heroArr++;
+// }
+
+
+let score=11;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<=10);
+
