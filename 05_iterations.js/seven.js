f1041696dca24fefa7315a8f037dd9a075f5a487
manpreetjord <30529028+manpreetjord@users.noreply.github.com>
2023-09-29 19:32:51 +0000

    filter map reduce

---
+++ 05_iterations.js/seven.js
@@ -0,0 +1,5 @@
+//Reduce
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+
+const newNums=myNums.map((num)=>{return num+10})
+console.log(newNums);
--- 05_iterations.js/six.js
+++ 05_iterations.js/six.js
@@ -1,1 +1,16 @@
-const coding=["js","ruby","python","cpp","java"]
+// const coding=["js","ruby","python","cpp","java"]
+
+
+
+// const values=coding.forEach((item)=>{
+//     console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+const newNums=myNums.filter((num)=>{ return num>5;
+   
+})
+console.log(newNums)
