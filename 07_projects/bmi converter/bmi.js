47a9d690a2b8f2b0f81370611aad0ca8e9e221ff
manpreetjord <30529028+manpreetjord@users.noreply.github.com>
2023-10-10 18:59:44 +0000

    bmi

--- 07_projects/bmi converter/bmi.js
+++ 07_projects/bmi converter/bmi.js
@@ -1,1 +1,28 @@
-console.log("BMI Converter")
+const form=document.querySelector("form");
+
+form.addEventListener('submit',function (e){
+    e.preventDefault();
+    const height=parseInt(document.querySelector("#height").value)
+    const weight=parseInt(document.querySelector("#weight").value)
+    const results=document.querySelector('#results')
+})
+
+if (height==="" ||height < 0 ||isNaN(height)) {
+    results.innerHTML=`Please give a valid height ${height}`
+}else
+if (weight==="" ||weight < 0 ||isNaN(weight)) {
+    results.innerHTML=`Please give a valid height ${weight}`
+}
+// }
+// console.log("BMI Converter")
+// const height=document.querySelector("#height").innerText
+// console.log("Height",height);
+// const weight=document.querySelector("#weight").innerText
+// console.log("Weight",weight);
+
+const calculate=()=>{
+    const bmi=Math.ceil((weight) / (height*height));
+    console.log
+    console.log("BMI is ",bmi)
+
+}
