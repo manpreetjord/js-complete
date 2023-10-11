051e9ebdb4cda17c750712d0a469cadb08bece7d
manpreetjord <30529028+manpreetjord@users.noreply.github.com>
2023-10-11 06:57:16 +0000

    bmi complete

--- 07_projects/bmi converter/bmi.js
+++ 07_projects/bmi converter/bmi.js
@@ -5,24 +5,21 @@ form.addEventListener('submit',function (e){
     const height=parseInt(document.querySelector("#height").value)
     const weight=parseInt(document.querySelector("#weight").value)
     const results=document.querySelector('#results')
-})
+
 
 if (height==="" ||height < 0 ||isNaN(height)) {
     results.innerHTML=`Please give a valid height ${height}`
-}else
-if (weight==="" ||weight < 0 ||isNaN(weight)) {
+}else if (weight==="" ||weight < 0 ||isNaN(weight)) {
     results.innerHTML=`Please give a valid height ${weight}`
+}else {
+    const bmi=(weight/(height*height/10000)).toFixed(2)
+    if(bmi<18.6){
+    results.innerHTML=`<span>Your are underweight ${bmi}</span>`
+}else if (bmi>18.6 && bmi<24.9) {
+    results.innerHTML=`<span>Your are in Normal Range ${bmi}</span>`
+    
+} else if (bmi>24.9){
+    results.innerHTML=`<span>Your are Overweight ${bmi}</span>`
 }
-// }
-// console.log("BMI Converter")
-// const height=document.querySelector("#height").innerText
-// console.log("Height",height);
-// const weight=document.querySelector("#weight").innerText
-// console.log("Weight",weight);
-
-const calculate=()=>{
-    const bmi=Math.ceil((weight) / (height*height));
-    console.log
-    console.log("BMI is ",bmi)
-
 }
+});
